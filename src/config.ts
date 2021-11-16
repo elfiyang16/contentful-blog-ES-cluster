0e735a11be1be44b04c3d2e91c3cf840f1bb2964
Elfi_Y <elfiyang16@gmail.com>
2021-11-16 14:12:42 +0000

    swtich to ts

---
+++ src/config.ts
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import { Client } from "@elastic/elasticsearch";
+const createAwsElasticsearchConnector = require("aws-elasticsearch-connector");
+
+const REGION = process.env.AWS_ACCESS_KEY_ID;
+const AWS_ACCESS_KEY_ID = process.env.AWS_ACCESS_KEY_ID;
+const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY;
+
+if (!AWS_ACCESS_KEY_ID || !AWS_SECRET_ACCESS_KEY) {
+  throw new Error(`Error getting AWS credentials`);
+}
+
+AWS.config.update({
+  accessKeyId: AWS_ACCESS_KEY_ID,
+  secretAccessKey: AWS_SECRET_ACCESS_KEY,
+  region: REGION,
+});
+
+export const createElasticSearchClient = () => {
+  const client = new Client({
+    ...createAwsElasticsearchConnector(AWS.config),
+    node: process.env.ES_ENDPOINT ?? `http://localhost:9200`,
+    maxRetries: 5,
+    requestTimeout: 30000,
+  });
+
+  return client;
+};
